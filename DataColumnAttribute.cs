cb2a473bc3009943f7fb8d7ede6c7baa9a773f51
jaw118 <46105391+jaw118@users.noreply.github.com>
2018-12-26 06:18:04 +0700

    Add files via upload

---
+++ DataColumnAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DKSH.MailAgent.Attributes
+{
+
+    public class DataColumnAttribute : Attribute
+    {
+        public string Name { get; }
+        public DataColumnAttribute(string name)
+        {
+            Name = name;
+        }
+    }
+}
